ae01da2ad0e7d09805b49f49247b9d652e06c96a
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-12 20:54:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
-alert("Bem vindo ao jogo de numero secreto!");
-let numeroSecreto = 29
+// alert("Bem vindo ao jogo de numero secreto!");
+// let numeroSecreto = 29;
+// let chute = prompt("Escolha um número entre 1 e 30:");
+
+// if (numeroSecreto == chute) {
+//     alert("Você acertou!")
+// } else {
+//     alert("Você errou!")
+// }
+
+// DESAFIO 01
+// Atribuições de valores
+let lua = "Lua"
+let idade = 25
+let numeroDeVendas = 50
+let saldoDisponivel = 1000
+let mensagemDeErro = "Erro! Preencha todos os campos"
+
+// Alertas
+alert("Boas vindas ao meu jogo!")
+alert(mensagemDeErro)
+
+//Interação com usuario.
+let nome = prompt("Qual o seu nome?")
+let idade2 = prompt("Qual sua idade?")
+if (idade2 >= 18) {
+    alert("Pode tirar a habilitação!"); 
+} else {
+    alert("Não pode tirar a habilitação ainda!")
+}
