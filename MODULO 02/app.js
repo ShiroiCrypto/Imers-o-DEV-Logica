d7ef1cb7461171dfbed617060eb98878cf9aaca6
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-13 11:24:43 -0300

    Update app.js

--- MODULO 02/app.js
+++ MODULO 02/app.js
@@ -1,7 +1,43 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+// alert("Boas vindas ao jogo do número secreto");
+// let numeroSecreto = 4;
+// console.log(numeroSecreto)
+// let chute = prompt("Escolha um número entre 1 e 10");
+// console.log(`Escolheu: ${chute}!`)
 
-if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+// if (chute == numeroSecreto) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+//     console.log("Mais de 8000 de QI!")
+// } else {
+//     alert("Você é um beta, acabou errando.")
+//     console.log("Betinha errou!")
+// }
+
+// ATIVIDADE 01
+
+/*
+Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+*/
+
+// alert("Bem vindo ao meu site!")
+// let dia = prompt("Qual dia é hoje?")
+// let diaEscolhido = "Sabado"
+// let diaEscolhido2 = "Domingo"
+
+// if (dia == diaEscolhido || diaEscolhido2){
+//     alert("Bom final de semana!")
+// } else {
+//     alert("Hoje tem aula ou já teve infelizmente.")
+// }
+
+// ATIVIDADE 02
+
+/*
+Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+*/
+let numero = prompt("Digite um numero:")
+
+if (numero < 0){
+    alert("Seu numero é negativo.")
+} else {
+    alert("Seu numero é positivo.")
 }
