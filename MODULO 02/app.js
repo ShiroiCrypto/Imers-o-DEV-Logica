a3e6ea4940be377baf8ea14ccc4f870e386c9c08
Shiroi | Crypto <93323888+ShiroiCrypto@users.noreply.github.com>
2024-11-14 10:56:16 -0300

    Update app.js

--- MODULO 02/app.js
+++ MODULO 02/app.js
@@ -34,10 +34,29 @@ Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Do
 /*
 Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
 */
-let numero = prompt("Digite um numero:")
+// let numero = prompt("Digite um numero:")
 
-if (numero < 0){
-    alert("Seu numero é negativo.")
-} else {
-    alert("Seu numero é positivo.")
-}
+// if (numero < 0){
+//     alert("Seu numero é negativo.")
+// } else {
+//     alert("Seu numero é positivo.")
+// }
+
+// ATIVIDADE 03
+
+/*
+Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+*/
+// let pontos = 0
+// let numero = 10
+
+// alert("Bem Vindo ao jogo de adivinhação!")
+// alert("Tente acertar um numero entre 1 e 10!")
+
+// let chute = prompt("Digite um numero:")
+
+// if (chute == numero){
+//     alert("Parabéns você acertou o número!")
+// } else {
+//     alert("Infelizmente você errou o número :(")
+// }
